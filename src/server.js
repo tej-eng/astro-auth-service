a611b347035d558b8b5b0975f3ddc7e7c861f607
tejpal singh <tej@dhwaniastro.com>
2026-06-05 13:08:45 +0530

    change for get request origin main

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.use(cors({
     origin: ["http://localhost:7001", "https://astro-panel-satge-git-main-tej-engs-projects.vercel.app","https://astro-panel-satge.vercel.app"],
   credentials: true,
 }));
-
+  console.log("request origin:", app.get("origin"));
   app.use(express.json());
   app.use(cookieParser());
   app.use(rateLimiter);
