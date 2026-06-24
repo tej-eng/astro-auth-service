4705aa435c309bb805d4803ab66103986a0c01ee
tejpal singh <tej@dhwaniastro.com>
2026-06-24 15:35:40 +0530

    change for token

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -6,8 +6,7 @@ export const generateAccessToken = (user) => {
   return jwt.sign(
     { id: user.id },
     process.env.JWT_SECRET,
-     { expiresIn: "1d" }
-     // { expiresIn: "2m" }
+    { expiresIn: "1d" }
   );
 };
 
